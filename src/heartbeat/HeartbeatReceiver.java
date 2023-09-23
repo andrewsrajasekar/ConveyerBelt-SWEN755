41cc6565309dfde0b522f41b5ab9cbd7b8cf3248
andrewsrajasekar <andrewsrajasekar@gmail.com>
2023-09-23 18:02:57 -0400

    HeartBeat Receiver and Fault Monitor Changes

--- src/heartbeat/HeartbeatReceiver.java
+++ src/heartbeat/HeartbeatReceiver.java
@@ -6,7 +6,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class HeartbeatReceiver {
-    private static HashMap<Integer, Long> sendIndexVsLastUpdatedTimeStamp = new HashMap<>();
+    private static HashMap<Integer, Long> senderIdVsLastUpdatedTimeStamp = new HashMap<>();
     private static Long senderFreq = 2000l;
     private static Long senderCheckFreq = senderFreq + 500l;
 
@@ -30,15 +30,15 @@ public class HeartbeatReceiver {
     }
 
     private static void updateTime(int id, Long updatedMilliseconds) {
-        sendIndexVsLastUpdatedTimeStamp.put(id, updatedMilliseconds);
+        senderIdVsLastUpdatedTimeStamp.put(id, updatedMilliseconds);
     }
 
     private static boolean checkAlive() {
         Long currentMillisecond = System.currentTimeMillis();
-        Iterator<Integer> sendIndexVsLastUpdatedTimeStampIter = sendIndexVsLastUpdatedTimeStamp.keySet().iterator();
+        Iterator<Integer> sendIndexVsLastUpdatedTimeStampIter = senderIdVsLastUpdatedTimeStamp.keySet().iterator();
         while (sendIndexVsLastUpdatedTimeStampIter.hasNext()) {
             Integer id = sendIndexVsLastUpdatedTimeStampIter.next();
-            Long lastUpdatedTimeStamp = sendIndexVsLastUpdatedTimeStamp.get(id);
+            Long lastUpdatedTimeStamp = senderIdVsLastUpdatedTimeStamp.get(id);
             if ((currentMillisecond - (senderFreq + 1000l)) > lastUpdatedTimeStamp) {
                 FaultMonitor.notifyUser(id, lastUpdatedTimeStamp);
             }
